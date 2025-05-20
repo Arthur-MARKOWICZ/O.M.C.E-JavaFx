4a816ddccc767139d69109af64130813764ddec4
Arthur-MARKOWICZ <164639904+Arthur-MARKOWICZ@users.noreply.github.com>
2025-05-20 08:55:04 -0300

    primeiro commit

---
+++ src/main/java/br/pucpr/omcejavafx/HelloController.java
@@ -0,0 +1,14 @@
+package br.pucpr.omcejavafx;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
