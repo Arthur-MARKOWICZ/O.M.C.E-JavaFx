17182cc232964eeef7b04dbcb0c440e217698a5b
GustavoTF06 <gustavotissot06@gmail.com>
2025-05-23 14:25:32 -0300

    criando classe do produto

---
+++ src/main/java/br/pucpr/omcejavafx/Produto.java
@@ -0,0 +1,11 @@
+package br.pucpr.omcejavafx;
+
+public class Produto {
+    private long id;
+    private String nome;
+    private Double preco;
+    private String detalhes;
+    private boolean vendido;
+    private byte[] imagem;
+    private String imageTipo;
+}
