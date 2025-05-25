666230d47b82f868d23e2ff3ebb09592ed320544
Luigi Morgenstern Milani <luigimilani2003@outlook.com>
2025-05-25 11:58:56 -0300

    Create Usuario.java

---
+++ src/main/java/br/pucpr/omcejavafx/Usuario.java
@@ -0,0 +1,16 @@
+package br.pucpr.omcejavafx;
+
+public class Usuario {
+    private long id;
+    private String nome;
+    private String nomeusuario;
+    private Int cpf;
+    private String senha;
+    private String sexo;
+    private String datadenascimento;
+    private Int telefone;
+    private String email;
+    private String endereco;
+    private String cep;
+    private Boolean ativo;
+}
