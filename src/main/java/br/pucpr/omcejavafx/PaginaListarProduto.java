b66e192b0ef715f8dc3b0903a3d9b35e52a2f157
GustavoTF06 <gustavotissot06@gmail.com>
2025-06-13 11:20:20 -0300

    botao voltar PaginaListarProduto.java

--- src/main/java/br/pucpr/omcejavafx/PaginaListarProduto.java
+++ src/main/java/br/pucpr/omcejavafx/PaginaListarProduto.java
@@ -18,6 +18,12 @@ public class PaginaListarProduto extends Application {
 
     @Override
     public void start(Stage stage) {
+        Button voltarBtn = new Button("Voltar");
+        voltarBtn.setOnAction(e -> {
+            PaginaEscolherCrudProduto menu = new PaginaEscolherCrudProduto();
+            menu.voltarMenuProduto(stage);
+        });
+
         TableView<Produto> tabela = new TableView<>();
         ObservableList<Produto> produtos = FXCollections.observableArrayList();
 
@@ -26,20 +32,20 @@ public class PaginaListarProduto extends Application {
                 new javafx.beans.property.SimpleStringProperty(String.valueOf(cellData.getValue().getId())));
 
         TableColumn<Produto, String> nomeColuna = new TableColumn<>("Nome");
-        nomeColuna.setCellValueFactory(cellData -> new javafx.beans.property.SimpleStringProperty
-                (cellData.getValue().getNomeProduto()));
+        nomeColuna.setCellValueFactory(cellData ->
+                new javafx.beans.property.SimpleStringProperty(cellData.getValue().getNomeProduto()));
 
         TableColumn<Produto, String> precoColuna = new TableColumn<>("Preço");
-        precoColuna.setCellValueFactory(cellData -> new javafx.beans.property.SimpleStringProperty
-                ("R$ " + String.format("%.2f", cellData.getValue().getPreco())));
+        precoColuna.setCellValueFactory(cellData ->
+                new javafx.beans.property.SimpleStringProperty("R$ " + String.format("%.2f", cellData.getValue().getPreco())));
 
         TableColumn<Produto, String> condicaoColuna = new TableColumn<>("Condição");
-        condicaoColuna.setCellValueFactory(cellData -> new javafx.beans.property.SimpleStringProperty
-               (cellData.getValue().getCondicao()));
+        condicaoColuna.setCellValueFactory(cellData ->
+                new javafx.beans.property.SimpleStringProperty(cellData.getValue().getCondicao()));
 
         TableColumn<Produto, String> categoriaColuna = new TableColumn<>("Categoria");
-        categoriaColuna.setCellValueFactory(cellData -> new javafx.beans.property.SimpleStringProperty
-                (cellData.getValue().getCategoria()));
+        categoriaColuna.setCellValueFactory(cellData ->
+                new javafx.beans.property.SimpleStringProperty(cellData.getValue().getCategoria()));
 
         TableColumn<Produto, ImageView> imagemColuna = new TableColumn<>("Imagem");
         imagemColuna.setCellValueFactory(cellData -> {
@@ -66,9 +72,13 @@ public class PaginaListarProduto extends Application {
         }
 
         BorderPane layout = new BorderPane();
-        layout.setCenter(tabela);
-        Scene scene = new Scene(layout, 800, 600);
 
+        VBox topo = new VBox(10, voltarBtn);
+        topo.setStyle("-fx-padding: 10; -fx-alignment: center-left;");
+        layout.setTop(topo);
+        layout.setCenter(tabela);
+
+        Scene scene = new Scene(layout, 800, 600);
         stage.setTitle("Lista de Produtos Cadastrados");
         stage.setScene(scene);
         stage.show();
